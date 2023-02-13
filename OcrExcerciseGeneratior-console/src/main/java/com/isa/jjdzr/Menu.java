78140aa5834ba3dbb7ecbc9a7f7a73ad34870c16
Piotr <pmanikowski93@gmail.com>
2023-02-13 16:43:03 +0000

    add sighUp method

--- OcrExcerciseGeneratior-console/src/main/java/com/isa/jjdzr/Menu.java
+++ OcrExcerciseGeneratior-console/src/main/java/com/isa/jjdzr/Menu.java
@@ -1,66 +1,54 @@
 package com.isa.jjdzr;
+
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
 
-public class Menu
-{
-    static Scanner in = new Scanner( System.in );
+public class Menu {
+    static Scanner in = new Scanner(System.in);
 
-    static void nowyUzytkownik()
-    {
-        System.out.println( "Wybrano tworzenie nowego użytkownika" );
+    static void nowyUzytkownik() {
+        SignUp.createUser();
     }
 
-    static void logowanieUzytkownika()
-    {
-        System.out.println( "Wybrano opcję zalogowania" );
+    static void logowanieUzytkownika() {
+        System.out.println("Wybrano opcję zalogowania");
     }
-    static void bezLogowania()
-    {
-        System.out.println( "Wybrano opcję bez zalogowania" );
+
+    static void bezLogowania() {
+        System.out.println("Wybrano opcję bez zalogowania");
     }
-    static void pokazMenu()
-    {
+
+    static void pokazMenu() {
         System.out.println("     ****************************************");
         System.out.println("     *                 MENU                 *");
         System.out.println("     ****************************************");
 
-        System.out.print( "Wybierz opcje:\n1. Tworzenie użytkownika\n2. Logowanie\n3. Bez zalogowania\n4. Koniec\n>> " );
+        System.out.print("Wybierz opcje:\n1. Tworzenie użytkownika\n2. Logowanie\n3. Bez zalogowania\n4. Koniec\n>> ");
     }
 
-    static void menu()
-    {
+    static void menu() {
         try {
-        int nrOpcji;
-        do {
-            pokazMenu();
-            nrOpcji = in.nextInt();
-            switch (nrOpcji) {
-                case 1:
-                    nowyUzytkownik();
-                    break;
-                case 2:
-                    logowanieUzytkownika();
-                    break;
-                case 3:
-                    bezLogowania();
-                    break;
-
-
+            int nrOpcji;
+            do {
+                pokazMenu();
+                nrOpcji = in.nextInt();
+                switch (nrOpcji) {
+                    case 1 -> nowyUzytkownik();
+                    case 2 -> logowanieUzytkownika();
+                    case 3 -> bezLogowania();
+                }
             }
-        }
 
-        while( nrOpcji != 4 );
+            while (nrOpcji != 4);
 
-            } catch (InputMismatchException e) {
+        } catch (InputMismatchException e) {
             System.out.println("Niepoprawna opcja");
         }
 
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         menu();
     }
 }
