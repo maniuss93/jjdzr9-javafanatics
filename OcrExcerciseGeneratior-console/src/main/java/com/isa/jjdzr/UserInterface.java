6158c78a4b661e92b53402a88234592c8520eb67
Piotr Jackowski <jackowskipiotr@o2.pl>
2023-02-14 23:20:01 +0100

    Beginning of work on Random Exercise Generator

--- OcrExcerciseGeneratior-console/src/main/java/com/isa/jjdzr/UserInterface.java
+++ OcrExcerciseGeneratior-console/src/main/java/com/isa/jjdzr/UserInterface.java
@@ -1,5 +1,75 @@
 package com.isa.jjdzr;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UserInterface {
-    String userInterfaceInfo = "This will be the User Interface Class";
+
+    Scanner scanner = new Scanner(System.in);
+    AdvancementLevelForm advancementLevelForm = new AdvancementLevelForm();
+
+    void printAdvancementLevel(){
+        if (advancementLevelForm.getUserAdvancementLevel() == 50){
+            System.out.println("POCZĄTKUJĄCY");
+            } else if (advancementLevelForm.getUserAdvancementLevel() == 100){
+            System.out.println("ZAAWANSOWANY");
+        }else if (advancementLevelForm.getUserAdvancementLevel() == 150) {
+            System.out.println("PROFESJONALNY");
+        }else {
+            System.out.println("BRAK");
+        }
+
+
+    }
+    void showUserInterfaceMenu() {
+        System.out.println("     ****************************************");
+        System.out.println("     *        USER INFORMATION PANEL        *");
+        System.out.println("     ****************************************");
+        System.out.print("     *     TWÓJ POZIOM ZAAWANSOWANIA TO: "    );
+        printAdvancementLevel();
+
+        System.out.print("Wybierz opcje:\n1. Test poziomu zaawansowania\n2. Wygeneruj losowy zestaw ćwiczeń\n3. Zobacz historie treningów\n4. Dodaj ćwiczenie\n5.Koniec\n>>");
+    }
+
+    void userInterfaceMenu() {
+        try {
+            int optionNumber;
+            do {
+                showUserInterfaceMenu();
+                optionNumber = scanner.nextInt();
+                switch (optionNumber) {
+                    case 1 -> takeAdvancementTest();
+                    case 2 -> generateExerciseSet();
+                    case 3 -> showTrainingHistory();
+                    case 4 -> addUserExercise();
+                }
+            }
+
+            while (optionNumber != 5);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Niepoprawna opcja");
+        }
+
+    }
+
+    private void addUserExercise() {
+        System.out.println("Wybrano opcję DODAJ ĆWICZENIE");
+    }
+
+    private void showTrainingHistory() {
+        System.out.println("Wybrano opcję POKAŻ HISTORIĘ ĆWICZEŃ");
+    }
+
+    private void generateExerciseSet() {
+
+    }
+
+    private void takeAdvancementTest() {
+        advancementLevelForm.advancementLevelMenu();
+    }
+
+    public void main(String[] args) {
+        userInterfaceMenu();
+    }
 }
--- OcrExcerciseGeneratior-console/src/main/java/com/isa/jjdzr/WelcomePage.java
+++ OcrExcerciseGeneratior-console/src/main/java/com/isa/jjdzr/WelcomePage.java
@@ -2,4 +2,5 @@ package com.isa.jjdzr;
 
 public class WelcomePage {
     String welcomePageInfo = "This will be the Welcome Page Class";
+
 }
