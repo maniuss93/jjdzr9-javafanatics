e7e32af814b35ae97ca9f7b15dbce3227516c55d
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-03-21 17:58:06 +0100

    GeneratePDF class code add

--- OcrExerciseGenerator-console/src/main/java/com/isa/jjdzr/App.java
+++ OcrExerciseGenerator-console/src/main/java/com/isa/jjdzr/App.java
@@ -1,8 +1,11 @@
 package com.isa.jjdzr;
 
+import static com.isa.jjdzr.exercise.service.GeneratePDF.generatePdf;
+
 public class App {
     public static void main(String[] args) {
-        JavaFanaticsTerminalLogo.javaFanaticsLogo();
-        Menu.menu();
+//        JavaFanaticsTerminalLogo.javaFanaticsLogo();
+//        Menu.menu();
+        generatePdf();
     }
 }
--- OcrExerciseGenerator-core/src/main/java/com/isa/jjdzr/exercise/service/GeneratePDF.java
+++ OcrExerciseGenerator-core/src/main/java/com/isa/jjdzr/exercise/service/GeneratePDF.java
@@ -1,5 +1,32 @@
 package com.isa.jjdzr.exercise.service;
 
-public class GeneratePDF {
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GeneratePDF {
+    public static void generatePdf() {
+        try {
+            Document document = new Document();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd_HHmmss");
+            String id = formatter.format(new Date());
+            String fileName = "exercise" + id + ".pdf";
+            String filePath = Paths.get("OcrExerciseGenerator-core","src","main","java","com","isa","jjdzr","resourcesPDF", fileName).toString();
+            OutputStream outputStream = new FileOutputStream(new File(filePath));
+            PdfWriter.getInstance(document, outputStream);
+            document.open();
+            document.add(new Paragraph("Hello World!"));
+            document.close();
+            System.out.println("The PDF file has been created.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
