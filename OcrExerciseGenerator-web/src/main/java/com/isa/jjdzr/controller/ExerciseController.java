76bcc625076df17691b82e1fe28841dba4f5c9ed
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-03-29 17:48:46 +0200

    It is alive :)

--- OcrExerciseGenerator-web/src/main/java/com/isa/jjdzr/controller/ExerciseController.java
+++ OcrExerciseGenerator-web/src/main/java/com/isa/jjdzr/controller/ExerciseController.java
@@ -1,9 +1,13 @@
 package com.isa.jjdzr.controller;
 
+import com.isa.jjdzr.exercise.service.PdfExerciseGenerator;
 import com.isa.jjdzr.service.ExerciseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,14 +17,14 @@ public class ExerciseController {
 
     private final ExerciseService exerciseService;
 
-    @GetMapping(path = "/generate-pdf")
-    public String generatePdf(){
-        try {
-            exerciseService.generatePdf();
-        }catch (Exception e){
-            System.out.println("Could not generate pdf ! :(\n" + e.getMessage());
-        }
-        return "home";
+    @Autowired
+    private PdfExerciseGenerator pdfExerciseGenerator;
+
+    @GetMapping("/generate-pdf")
+    public ResponseEntity<byte[]> generatePdf() throws Exception {
+        return pdfExerciseGenerator.generatePdf();
     }
+
+
 
 }
