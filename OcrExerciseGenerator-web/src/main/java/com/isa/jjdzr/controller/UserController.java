cad9282f2676ea569f252b781595f1c82cda518d
Piotr <pmanikowski93@gmail.com>
2023-04-14 21:29:31 +0100

    Complete edit user details and account delete options

--- OcrExerciseGenerator-web/src/main/java/com/isa/jjdzr/controller/UserController.java
+++ OcrExerciseGenerator-web/src/main/java/com/isa/jjdzr/controller/UserController.java
@@ -15,6 +15,9 @@ import java.util.Optional;
 @Controller
 @RequestMapping("user")
 public class UserController {
+    private static final String userEditDetails = "user-edit-details";
+    private static final String userCreateForm = "user-create-form";
+    private static final String userEditPassword = "user-edit-password";
 
     private final UserService userService;
 
@@ -26,18 +29,24 @@ public class UserController {
     @GetMapping("/new")
     public String getCreateUserForm(Model model) {
         model.addAttribute("user", new User());
-        return "user-create-form";
+        return userCreateForm;
     }
 
     //localhost:8080/user/new
     @PostMapping(value = "/new")
-    public String createUser(@ModelAttribute("user") User user, Model model) {
+    public String createUser(@ModelAttribute("user") User user,
+                             @RequestParam("userPassword") String userPassword,
+                             @RequestParam("confirmPassword") String confirmPassword,
+                             Model model) {
         if (userService.existsByName(user.getUserName())) {
             model.addAttribute("usernameAlreadyTaken", "Ta nazwa użytkownika jest zajęta");
-            return "user-create-form";
+            return userCreateForm;
         } else if (userService.existsByEmail(user.getUserEmail())) {
             model.addAttribute("userEmailAlreadyTaken", "Ten address email jest zajęty");
-            return "user-create-form";
+            return userCreateForm;
+        } else if (!userPassword.equals(confirmPassword)) {
+            model.addAttribute("userPasswordsDoNotMatch", "Hasła muszą być identyczne");
+            return userCreateForm;
         } else {
             model.addAttribute("user", userService.createUser(user));
             return "redirect:/";
@@ -63,41 +72,75 @@ public class UserController {
         return "redirect:/user/" + id + "/userpanel";
     }
 
-    @GetMapping("/{id}/editdetails")
-    public String getUserProfileEditForm(@PathVariable Long id, Model model) {
-        User user = userService.findByUserId(id);
-        model.addAttribute("user", user);
-        return "user-edit-details";
-    }
-    @GetMapping("/{id}/editpassword")
-    public String getUserPasswordEditForm(@PathVariable Long id, Model model){
-        User user = userService.findByUserId(id);
-        model.addAttribute("user", user);
-        return "user-edit-password";
-    }
-
-    @PostMapping(value = "/{id}/edit")
-    public String userProfileEdit(@Valid @ModelAttribute("user") User user, @PathVariable Long id, Model model) {
-        User byUserId = userService.findByUserId(id);
-        List <User> allUsers = userService.findAllUsers();
-        allUsers.remove(byUserId);
-        if (allUsers.stream().map(User::getUserName).toList().contains(user.getUserName())) {
-            model.addAttribute("usernameAlreadyTaken", "Ta nazwa użytkownika jest zajęta");
-            return "user-edit-details";
-        } else if (allUsers.stream().map(User::getUserEmail).toList().contains(user.getUserEmail())) {
-            model.addAttribute("userEmailAlreadyTaken", "Ten address email jest zajęty");
-            return "user-edit-details";
-        } else {
-            userService.editUser(user);
-            return "redirect:/user/" + id + "/userpanel";
-        }
-    }
-
     @GetMapping("/{id}/userpanel")
     public String showUserProfile(@PathVariable Long id, Model model) {
         User user = userService.findByUserId(id);
         model.addAttribute("user", user);
         return "user-panel";
+    }
+
+    @GetMapping("/{id}/editdetails")
+    public String getUserProfileEditForm(@PathVariable Long id, Model model) {
+        User user = userService.findByUserId(id);
+        model.addAttribute("user", user);
+        return userEditDetails;
+    }
+
+    @PostMapping(value = "/{id}/edit")
+    public String userProfileEdit(@Valid @ModelAttribute("user") User user,
+                                  @PathVariable Long id,
+                                  Model model) {
+        User userFromDb = userService.findByUserId(id);
+        List<User> allUsers = userService.findAllUsers();
+        allUsers.remove(userFromDb);
+        if (allUsers.stream().map(User::getUserName).toList().contains(user.getUserName())) {
+            model.addAttribute("usernameAlreadyTaken", "Ta nazwa użytkownika jest zajęta");
+            return userEditDetails;
+        } else if (allUsers.stream().map(User::getUserEmail).toList().contains(user.getUserEmail())) {
+            model.addAttribute("userEmailAlreadyTaken", "Ten address email jest zajęty");
+            return userEditDetails;
+        } else {
+            userService.editUser(user);
+        }
+        return "redirect:/user/" + id + "/userpanel";
+    }
+
+    @GetMapping("/{id}/editpassword")
+    public String getUserPasswordEditForm(@PathVariable Long id, Model model) {
+        User user = userService.findByUserId(id);
+        model.addAttribute("user", user);
+        return userEditPassword;
+    }
+
+    @PostMapping(value = "/{id}/editpassword")
+    public String userPasswordEdit(@Valid @ModelAttribute("user") User user,
+                                   @PathVariable Long id,
+                                   @RequestParam("userCurrentPassword") String userCurrentPassword,
+                                   @RequestParam("userPassword") String userPassword,
+                                   @RequestParam("confirmPassword") String confirmPassword,
+                                   Model model) {
+        User userFromDb = userService.findByUserId(id);
+        if (!userCurrentPassword.equals(userFromDb.getUserPassword())) {
+            model.addAttribute("wrongPassword", "Twoje aktualne hasło jest nie poprawne");
+            return userEditPassword;
+        } else if (!userPassword.equals(confirmPassword)) {
+            model.addAttribute("userPasswordsDoNotMatch", "Hasła muszą być identyczne");
+            return userEditPassword;
+        } else {
+            userService.editUser(user);
+        }
+        return "redirect:/user/" + id + "/userpanel";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/confirmdelete")
+    public String confirmDeleteUser(@PathVariable Long id){
+        return "user-confirm-delete";
     }
 
     @ModelAttribute("availableUserAdvancementLevel")
