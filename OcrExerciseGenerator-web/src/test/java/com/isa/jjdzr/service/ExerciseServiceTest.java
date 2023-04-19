b1b5077247d01a232fbb21765458bd9a75b4408a
Jacek <jacek.kujawa0909@gmail.com>
2023-04-19 13:07:28 +0200

    ExerciseServiceTest class add

---
+++ OcrExerciseGenerator-web/src/test/java/com/isa/jjdzr/service/ExerciseServiceTest.java
@@ -0,0 +1,48 @@
+package com.isa.jjdzr.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExerciseServiceTest {
+
+    @Test
+    void generatePdf() {
+    }
+
+    @Test
+    void addExercise() {
+    }
+
+    @Test
+    void findAllExercises() {
+    }
+
+    @Test
+    void findExerciseByName() {
+    }
+
+    @Test
+    void existsByExerciseName() {
+    }
+
+    @Test
+    void existsByUrl() {
+    }
+
+    @Test
+    void editExercise() {
+    }
+
+    @Test
+    void deleteExercise() {
+    }
+
+    @Test
+    void generateRandomExercises() {
+    }
+
+    @Test
+    void getExercisesByIds() {
+    }
+}
