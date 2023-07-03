18db845d9295934a8fcd94d9db51a57b2f416f20
Piotr <pmanikowski93@gmail.com>
2023-07-03 19:59:46 +0100

    add welcome page

--- OcrExerciseGenerator-web/src/main/java/com/isa/jjdzr/controller/HomeController.java
+++ OcrExerciseGenerator-web/src/main/java/com/isa/jjdzr/controller/HomeController.java
@@ -1,11 +1,9 @@
 package com.isa.jjdzr.controller;
 
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,32 +11,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping()
 public class HomeController {
 
-    // http://localhost:8080
 
     @GetMapping
     public String getHome(@RequestParam(value = "logout", required = false) String logout, Model model) {
         if ("true".equals(logout)) {
             model.addAttribute("message", "Wylogowano pomyślnie");
         }
-        return "index"; // zwracamy nazwę widoku
+        return "index";
     }
 
-    // http://localhost:8080/info
-    @GetMapping("/info")
-    public String getInfo() {
-        return "info";
-    }
-
-    // http://localhost:8080/info
     @GetMapping("/calendar")
     public String getCalendar() {
         return "calendar";
-    }
-
-    // http://localhost:8080/demo
-    @GetMapping("/demo")
-    public String getDemo() {
-        return "demo-exercises";
     }
 
     @GetMapping("/admin")
